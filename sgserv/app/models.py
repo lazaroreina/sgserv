3445bdaea1bb0a6c1a385b417460b4e20a12a510
lazaroreina <debian.gnu.lazaro@gmail.com>
2021-06-07 21:39:58 -0300

    0.0.0.2

--- sgserv/app/models.py
+++ sgserv/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Endereco(models.Model):
+    logradouro = models.CharField(max_length=40)
+    numero = models.IntegerField()
+    bairro = models.CharField(max_length= 20)
+    cidade = models.CharField(max_length= 20)
+    uf = models.CharField(max_length= 2)
+    cep = models.IntegerField()
+    
+    def __str__(self):
+        return (self.logradouro, self.bairro)
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=30)
+    telefone = models.BigIntegerField()
+    email = models.EmailField()
+    endereco = models.ForeignKey(Endereco, on_delete=CASCADE)
+   
+    def __str__(self):
+        return self.nome
--- sgserv/app/views.py
+++ sgserv/app/views.py
@@ -1,3 +1,18 @@
+from django.db.models import fields
+from django.forms import ModelForm
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+class ClienteForm(ModelForm):
+    class Meta: 
+        model = Cliente
+        fields = [ 
+            'nome', 'telefone', 'email', 'endereco',
+        ]
+
+def lista(request, template_name='lista.html'):
+    form = ClienteForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return render(request, template_name, {'form':form})
